bfd72af6b7b4912adc7e087a5f5e9eb1f9e05999
Eliott Martin <eliott.martin@orange.fr>
2018-02-08 16:02:31 +0100

    Save new dump dofiles

--- fish/config.fish
+++ fish/config.fish
@@ -15,12 +15,6 @@ set -g -x PAGER "most"
 
 set -g -x PATH $PATH /home/alpaga/bin
 
-set -g -x JAVA_HOME /usr/local/jdk1.8.0_151
-set -g -x PATH $PATH $JAVA_HOME/bin
-
-set -g -x ANDROID_HOME /home/alpaga/Documents/Softwars/Android/Sdk
-set -g -x PATH $PATH $ANDROID_HOME/tools
-set -g -x PATH $PATH $ANDROID_HOME/platform-tools
 ##
 # IMPORT
 ##
